4974287ba6f9008631cadf85b3d0844eaabf1205
PioneerX7 <vita-kopy@yandex.ru>
2022-11-28 18:13:15 +0300

    установка приложения, данные вводятся вручную

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Укажите, какая операционная система на вашем утройстве: введите 0 - если iOS, 1 - если Android");
+        int clientOS = scanner.nextInt();
+        System.out.println("Введите год выпуска вашего устройства");
+        int clientDeviceYear = scanner.nextInt();
+
+        if (clientOS == 0) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        } else {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        }
     }
 }
