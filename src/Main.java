ef53ac866525a515f1d5f8ac4955c6797d20d608
PioneerX7 <vita-kopy@yandex.ru>
2022-11-28 18:39:11 +0300

    добавлен бесконечный цикл

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Укажите, какая операционная система на вашем утройстве: введите 0 - если iOS, 1 - если Android");
-        int clientOS = scanner.nextInt();
-        System.out.println("Введите год выпуска вашего устройства");
-        int clientDeviceYear = scanner.nextInt();
+        while (true) {
 
-        if (clientOS == 0) {
-            if (clientDeviceYear >= 2015) {
-                System.out.println("Установите версию приложения для iOS по ссылке");
+            System.out.println("Укажите, какая операционная система на вашем утройстве: введите 0 - если iOS, 1 - если Android");
+            int clientOS = scanner.nextInt();
+
+            if (clientOS == 0) {
+                System.out.println("Введите год выпуска вашего устройства");
+                int clientDeviceYear = scanner.nextInt();
+                if (clientDeviceYear >= 2015) {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                } else {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                }
+            } else if (clientOS == 1) {
+                System.out.println("Введите год выпуска вашего устройства");
+                int clientDeviceYear = scanner.nextInt();
+                if (clientDeviceYear >= 2015) {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                } else {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                }
             } else {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            }
-        } else {
-            if (clientDeviceYear >= 2015) {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            } else {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                System.out.println("Введите корректные данные для продолжения");
             }
         }
     }
